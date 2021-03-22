927f3c8bcc74afb5ef3683e9ba75f6ff7a6238ea
Fahim Miah <fmiah10@stuy.edu>
2021-03-22 04:39:52 +0100

    updated code

---
+++ matrix.py
@@ -0,0 +1,96 @@
+"""
+A matrix will be an N sized list of 4 element lists.
+Each individual list will represent an [x, y, z, 1] point.
+For multiplication purposes, consider the lists like so:
+x0  x1      xn
+y0  y1      yn
+z0  z1  ... zn
+1  1        1
+"""
+import math
+
+def make_translate( x, y, z ):
+    t = new_matrix()
+    ident(t)
+    t[0][3] = x
+    t[1][3] = y
+    t[2][3] = z
+    return t
+
+def make_scale( x, y, z ):
+    s = new_matrix()
+    ident(s)
+    s[0][0] = x
+    s[1][1] = y
+    s[2][2] = z
+    return s
+
+def make_rotX( theta ):
+    rX = new_matrix()
+    ident(rX)
+    theta = theta * (math.pi/ 180)
+    rX[1][1] = math.cos(theta)
+    rX[1][2] = -math.sin(theta)
+    rX[2][1] = math.sin(theta)
+    rX[2][2] = math.cos(theta)
+    return rX
+
+def make_rotY( theta ):
+    rY = new_matrix()
+    ident(rY)
+    theta = theta * (math.pi / 180)
+    rY[0][0] = math.cos(theta)
+    rY[0][2] = math.sin(theta)
+    rY[2][0] = -math.sin(theta)
+    rY[2][2] = math.cos(theta)
+    return rY
+
+def make_rotZ( theta ):
+    rZ = new_matrix()
+    ident(rZ)
+    theta = theta * (math.pi/ 180)
+    rZ[0][0] = math.cos(theta)
+    rZ[0][1] = -math.sin(theta)
+    rZ[1][0] = math.sin(theta)
+    rZ[1][1] = math.cos(theta)
+    return rZ
+
+#print the matrix such that it looks like
+#the template in the top comment
+def print_matrix( matrix ):
+    s = ''
+    for r in range( len( matrix[0] ) ):
+        for c in range( len(matrix) ):
+            s+= str(matrix[c][r]) + ' '
+        s+= '\n'
+    print(s)
+
+#turn the paramter matrix into an identity matrix
+#you may assume matrix is square
+def ident( matrix ):
+    for r in range( len( matrix[0] ) ):
+        for c in range( len(matrix) ):
+            if r == c:
+                matrix[c][r] = 1
+            else:
+                matrix[c][r] = 0
+
+#multiply m1 by m2, modifying m2 to be the product
+#m1 * m2 -> m2
+def matrix_mult( m1, m2 ):
+    point = 0
+    for row in m2:
+        #get a copy of the next point
+        tmp = row[:]
+        for r in range(4):
+            m2[point][r] = (m1[0][r] * tmp[0] + m1[1][r] * tmp[1] + m1[2][r] * tmp[2] + m1[3][r] * tmp[3])
+        point+= 1
+
+
+def new_matrix(rows = 4, cols = 4):
+    m = []
+    for c in range( cols ):
+        m.append( [] )
+        for r in range( rows ):
+            m[c].append( 0 )
+    return m
